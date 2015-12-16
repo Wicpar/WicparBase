ebf8cb100ceb67e989821e414a4a06c5c8477b79
Wicpar <frederic.artus@laposte.net>
2015-12-16 19:25:57 +0100

    reworked the hierarchy. now all Hierarchical should either be a child or a root in the worldTree. please do not specify a loop in that scheme to avoid infinite loops

--- src/com/wicpar/wicparbase/utils/WTree.java
+++ src/com/wicpar/wicparbase/utils/WTree.java
@@ -1,5 +1,6 @@
 package com.wicpar.wicparbase.utils;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 /**
@@ -8,9 +9,26 @@ import java.util.LinkedList;
 public class WTree<T>
 {
 
-	private LinkedList<Node> roots = new LinkedList<>();
+	private Node root = new Node();
 
-	public int getDepth(Node node,T object)
+	public void getRoot()
+	{
+
+	}
+
+	private int getDepth(T object)
+	{
+		int i;
+
+		for (Node n : root.childs)
+		{
+			if ((i = getDepth(n, object)) >= 0)
+				return i + 1;
+		}
+		return -1;
+	}
+
+	private int getDepth(Node node,T object)
 	{
 		int i;
 
@@ -24,9 +42,75 @@ public class WTree<T>
 		return -1;
 	}
 
-	private class Node
+
+	private ArrayList<Node> getNodesForObject(T object)
+	{
+		ArrayList<Node> nodes = null;
+
+		for (Node n : root.childs)
+		{
+			if (nodes == null)
+				nodes = getNodesForObject(n, object);
+			else
+				nodes.addAll(getNodesForObject(n, object));
+		}
+		return nodes;
+	}
+
+	private ArrayList<Node> getNodesForObject(Node root, T object)
+	{
+		ArrayList<Node> nodes = null;
+		int i;
+
+		if (root.data == object)
+		{
+			nodes = new ArrayList<>();
+			nodes.add(root);
+			return nodes;
+		}
+		for (Node n : root.childs)
+		{
+			if (nodes == null)
+				nodes = getNodesForObject(n, object);
+			else
+				nodes.addAll(getNodesForObject(n, object));
+		}
+		return nodes;
+	}
+
+	public class Node
 	{
 		private LinkedList<Node> childs = new LinkedList<>();
-		private T data;
+		private LinkedList<Node> parents = new LinkedList<>();
+		public T data = null;
+
+		public Node()
+		{
+		}
+
+		public Node(T data)
+		{
+			this.data = data;
+		}
+
+		public void destroy()
+		{
+			for (Node parent : parents)
+			{
+				parent.childs.remove(this);
+			}
+			parents.clear();
+		}
+		public void unlink(Node parent)
+		{
+			parent.childs.remove(this);
+			parents.remove(parent);
+		}
+
+		public void link(Node parent)
+		{
+			parent.childs.add(this);
+			parents.add(parent);
+		}
 	}
 }
