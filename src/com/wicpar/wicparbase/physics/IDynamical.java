991fc76d856ce240ed75ed4d9af272cea6761795
Wicpar <frederic.artus@laposte.net>
2015-12-17 20:34:38 +0100

    reworked the hierarchy. now all Hierarchical should either be a child or a root in the worldTree. please do not specify a loop in that scheme to avoid infinite loops

---
+++ src/com/wicpar/wicparbase/physics/IDynamical.java
@@ -0,0 +1,9 @@
+package com.wicpar.wicparbase.physics;
+
+/**
+ * Created by Frederic on 17/12/2015 at 19:15.
+ */
+public interface IDynamical
+{
+	void Update(double delta);
+}
---
+++ src/com/wicpar/wicparbase/physics/IForce.java
@@ -0,0 +1,10 @@
+package com.wicpar.wicparbase.physics;
+
+/**
+ * Created by Frederic on 17/12/2015 at 19:11.
+ */
+public interface IForce
+{
+	void UpdateForce(double delta);
+
+}
