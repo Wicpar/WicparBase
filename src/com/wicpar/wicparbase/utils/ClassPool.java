78bc1c1e59a6c92ce5a4b3ed84dec175eab84dac
Wicpar <frederic.artus@laposte.net>
2015-12-18 18:15:26 +0100

    made a classpool to handle the class Hierarchy

---
+++ src/com/wicpar/wicparbase/utils/ClassPool.java
@@ -0,0 +1,96 @@
+package com.wicpar.wicparbase.utils;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * Created by Frederic on 18/12/2015 at 16:58.
+ */
+public class ClassPool
+{
+	private final LinkedList<Object> objects = new LinkedList<>();
+	private final HashMap<Class, LinkedList> classpools = new HashMap<>();
+	private boolean shouldUpdate = false;
+
+	public ClassPool()
+	{
+	}
+
+	public ClassPool(Class... Handles)
+	{
+		CreateClasses(Handles);
+	}
+
+	public void addClass(Object... c)
+	{
+		for (Object o : c)
+		{
+			objects.add(o);
+		}
+		shouldUpdate = true;
+	}
+
+	public void removeClass(Object... c)
+	{
+		for (Object o : c)
+		{
+			objects.remove(o);
+		}
+		shouldUpdate = true;
+	}
+
+	public void RelaodClasses()
+	{
+		for (Class aClass : classpools.keySet())
+		{
+			RelaodClass(aClass);
+		}
+		shouldUpdate = false;
+	}
+
+	public void UpdateClass(Updater updater, Class c, Object... params)
+	{
+		LinkedList<Object> tmp;
+			tmp = classpools.get(c);
+			if (tmp == null)
+			{
+				CreateClasses(c);
+				tmp = classpools.get(c);
+			}
+			for (Object o : tmp)
+			{
+				updater.Update(o, params);
+			}
+	}
+
+	private void RelaodClass(Class c)
+	{
+		LinkedList<Object> tmp;
+
+		tmp = classpools.get(c);
+		tmp.clear();
+		for (Object object : objects)
+		{
+			if (c.isInstance(object))
+			{
+				tmp.add(object);
+			}
+		}
+	}
+
+	private void CreateClasses(Class... c)
+	{
+		for (Class aClass : c)
+		{
+			classpools.put(aClass, new LinkedList());
+			RelaodClass(aClass);
+		}
+	}
+
+	public interface Updater
+	{
+		void Update(Object c, Object... params);
+	}
+
+}
