eb34236db052964fb3d3fb6acd6aaf95b0f70254
Wicpar <frederic.artus@laposte.net>
2015-12-19 23:57:46 +0100

    tweaking springs

--- src/com/wicpar/wicparbase/physics/system/Defaults/Spring.java
+++ src/com/wicpar/wicparbase/physics/system/Defaults/Spring.java
@@ -43,9 +43,15 @@ public class Spring extends Force
 		lastForce = stiffness * stretch;
 
 		if (physical == a)
-			physical.getVel().add(new Vector3d(new Vector3d().set(posB).sub(posA).normalize().mul(lastForce).add(new Vector3d(velB).sub(velA).mul(dampening))).div(physical.getMass()).mul(delta));
+		{
+			physical.getVel().add(new Vector3d(new Vector3d().set(posB).sub(posA).normalize().mul(lastForce)).div(physical.getMass()).mul(delta));
+			physical.getVel().add(new Vector3d(velB).sub(velA).mul(dampening));
+		}
 		if (physical == b)
-			physical.getVel().add(new Vector3d(new Vector3d().set(posA).sub(posB).normalize().mul(lastForce).add(new Vector3d(velA).sub(velB).mul(dampening))).div(physical.getMass()).mul(delta));
+		{
+			physical.getVel().add(new Vector3d(new Vector3d().set(posA).sub(posB).normalize().mul(lastForce)).div(physical.getMass()).mul(delta));
+			physical.getVel().add(new Vector3d(velA).sub(velB).mul(dampening));
+		}
 		return false;
 	}
 }
